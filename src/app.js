133f2a83809a9b6cb6d2f668b5f28c8718398fc7
Dani Cornejo <dcornejo.h@gmail.com>
2025-04-25 03:40:37 +0000

    Generador de nombres de dominios

--- src/app.js
+++ src/app.js
@@ -1,11 +1,25 @@
-import "bootstrap";
-import "./style.css";
+let pronoun = ['the', 'our'];
+let adj = ['great', 'big'];
+let noun = ['jogger', 'racoon', 'puedes', 'awesome'];
+let extensions = ['.com', '.net', '.us', '.io', '.es'];
 
+console.log(" Dominios normales:");
+for (let p of pronoun) {
+  for (let a of adj) {
+    for (let n of noun) {
+      for (let ext of extensions) {
+        console.log(p + a + n + ext);
+      }
+    }
+  }
+}
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+console.log("\n💡 Domain hacks:");
+for (let n of noun) {
+  for (let ext of extensions) {
+    if (n.endsWith(ext.replace('.', ''))) {
+      let base = n.slice(0, n.length - ext.length + 1);
+      console.log(base + ext);
+    }
+  }
+}
